49b92fa987697a1331310d3f805efc013603c476
29ramya <68226270+29ramya@users.noreply.github.com>
2020-07-15 19:57:25 +0530

    Q1 assignment

---
+++ Batch1/Day2/Q1.js
@@ -0,0 +1,13 @@
+alert("hello user");
+
+
+let age=prompt("what  is ur age");
+console.log(age);
+
+let name=prompt("please enter your name);
+console.log(name);
+
+let beginner=confirm("are you new to the course");
+console.log(beginner);
+
+alert(beginner);
