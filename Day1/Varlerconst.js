952191f6039e7505c0af146d931dc9385b63eeda
29ramya <68226270+29ramya@users.noreply.github.com>
2020-07-15 11:07:29 +0530

    Varlerconst assignment

---
+++ Day1/Varlerconst.js
@@ -0,0 +1,22 @@
+
+var a=5;
+console.log(a);
+var b= 6;
+console.log(b);
+var c=10;
+console.log(c);
+console.log({a,b,c});
+
+var d="null";
+console.log(d);
+     
+     
+  {
+    let name="prashanth";
+    console.log(name);
+  }
+  
+  
+  const numbers=5;
+  console.log(numbers);
+    
