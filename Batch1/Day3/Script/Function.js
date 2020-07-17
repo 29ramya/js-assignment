95de90b93fdf9725371a7c058ffcd046f8b4e544
29ramya <68226270+29ramya@users.noreply.github.com>
2020-07-17 09:23:47 +0530

    First Question

---
+++ Batch1/Day3/Script/Function.js
@@ -0,0 +1,12 @@
+let check=function(number){
+   if (number%2===0)
+ {
+   result=console.log("number is even");
+ }
+ else
+ {
+   result=console.log("number is odd");
+ }
+ return result;
+ }
+ check(prompt("enter a number:"));
